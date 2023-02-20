2e2c5d0f2fa4d01b5e7a250793d6c909224d0479
Daniel Gustafsson <gunida@outlook.com>
2023-02-20 15:04:56 +0100

    Using Python 3.10

--- randovania/gui/qt.py
+++ randovania/gui/qt.py
@@ -330,7 +330,7 @@ def run(args):
     import randovania.monitoring
     randovania.monitoring.client_init()
 
-    locale.setlocale(locale.LC_ALL, "")  # use system's default locale
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")  # use system's default locale
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 
     data_dir = args.custom_network_storage
